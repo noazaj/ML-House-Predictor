1c62cc562ebe030cd8d5b527c6e7e98403d2b205
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2023-09-20 09:58:42 -0400

    Import bs4 and create soup from HTML

--- utilities/parser.py
+++ utilities/parser.py
@@ -2,8 +2,17 @@
 the HTML contents of the webpage and parse it accordingly."""
 
 import json
+import logging
 import os
 from fetcher import fetcher
+
+# Configure logger to append to it everytime it is written to
+# Might make a small cleanup script for the log to wipe it after a certain time period
+logging.basicConfig(
+    filename='webScraper/logs/scrapeLogs.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filemode='a',
+    level=logging.DEBUG)
 
 # Get the absolute path of the directory containing the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +30,20 @@ location = config['location']
 home_type = config['home_type']
 price = config['price']
 
-data = fetcher(location, home_type, price)
+raw_data = fetcher(location, home_type, price)
 
-def parse():
-    pass
+def parse(data: bytes):
+    # Import BeautifulSoup and try to catch any errors in the process and log them to the 
+    # log file
+    try:
+        from bs4 import BeautifulSoup
+        logging.info(f"Successfully imported BeautifulSoup")
+    except ImportError as err:
+        logging.error('Import error: %s', err)
+
+    # Create the 'soup' from the contents of the HTML data
+    soup = BeautifulSoup(data, 'html5lib')
+    print(soup.prettify())
+
+# Test function
+parse(raw_data)
